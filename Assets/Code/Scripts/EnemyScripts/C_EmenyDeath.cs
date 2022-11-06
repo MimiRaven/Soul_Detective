836c360ec7751b3daeed546b5f59abeb61fbe2b5
CasasJc <jesuscasas@knights.ucf.edu>
2022-11-06 17:20:46 -0500

    JC Code
    Enemy Health implemented

--- Assets/Code/Scripts/EnemyScripts/C_EmenyDeath.cs
+++ Assets/Code/Scripts/EnemyScripts/C_EmenyDeath.cs
@@ -6,11 +6,21 @@ public class C_EmenyDeath : MonoBehaviour
 {
     public C_XpScore c_XpScore;
     private bool isColliding = true;
-    public EnemyHealth enemyHealth;
+    //public EnemyHealth enemyHealth;
+
+    public int MaxHealth;
+    public int PlayerCurrentHealth;
+
+    void Start()
+    {
+        PlayerCurrentHealth = MaxHealth;
+    }
 
     void Update()
     {
         isColliding = false;
+        
+        EnemyDeath();
     }
 
     void OnCollisionEnter(Collision col)
@@ -20,7 +30,7 @@ public class C_EmenyDeath : MonoBehaviour
 
         if (col.collider.tag == "weapon")
         {
-            EnemyDeath();
+            PlayerTakeDamage();
             //col.collider.GetComponent<EnemyHealth>().TakeDamage(1);
             // Destroy(gameObject);
             //script.TakeDamage(1);
@@ -30,18 +40,29 @@ public class C_EmenyDeath : MonoBehaviour
         if (col.collider.tag == "Active")
         {
             Debug.Log("Attack hit");
-            EnemyDeath();
+            PlayerTakeDamage();
         }
 
     }
 
+    void PlayerTakeDamage()
+    {
+        PlayerCurrentHealth -= 1;
+        
+    }
 
     void EnemyDeath()
     {
-        
-        c_XpScore.CurrentScore += 1;
+
+        if(PlayerCurrentHealth <= 0)
+        {
+            c_XpScore.CurrentScore += 1;
+            Destroy(gameObject);
+            
+        }
+
         //Destroy(gameObject);
-        enemyHealth.TakeDamage(1);
+        //enemyHealth.TakeDamage(1);
 
     }
 
