f428e6019415e0f7b0a05055799b29a28234e700
CasasJc <jesuscasas@knights.ucf.edu>
2022-11-07 08:33:32 -0500

    JC Code
    Added a field of view for the player and turned off enemy ai to help with testing

---
+++ Assets/Code/Editor/FieldOfViewEditor1.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(C_FeildOfView1))]
+public class FieldOfViewEditor1 : Editor
+{
+	void OnSceneGUI()
+	{
+		C_FeildOfView1 fow = (C_FeildOfView1)target;
+		Handles.color = Color.white;
+		Handles.DrawWireArc(fow.transform.position, Vector3.up, Vector3.forward, 360, fow.viewRadius);
+		Vector3 viewAngleA = fow.DirFromAngle(-fow.viewAngle / 2, false);
+		Vector3 viewAngleB = fow.DirFromAngle(fow.viewAngle / 2, false);
+
+		Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleA * fow.viewRadius);
+		Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleB * fow.viewRadius);
+
+		Handles.color = Color.red;
+		foreach (Transform visibleTarget in fow.visibleTargets)
+		{
+			Handles.DrawLine(fow.transform.position, visibleTarget.position);
+		}
+	}
+}
