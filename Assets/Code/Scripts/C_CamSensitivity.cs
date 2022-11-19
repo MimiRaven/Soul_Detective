1f95558ceb7527b8557b9f1e557f60c5d02c6bae
CasasJc <jesuscasas@knights.ucf.edu>
2022-11-19 10:25:21 -0500

    JC Code
    Cam sensitivity update

---
+++ Assets/Code/Scripts/C_CamSensitivity.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class C_CamSensitivity : MonoBehaviour
+{
+   //public CinemachineFreeLook cinemachineVirtualCamera;
+   //
+   //void Start()
+   //{
+   //    cinemachineVirtualCamera.m_XAxis.m_MaxSpeed = 250f;
+   //    cinemachineVirtualCamera.m_YAxis.m_MaxSpeed = 10f;
+   //}
+   //
+   //// Update is called once per frame
+   //void Update()
+   //{
+   //    
+   //}
+   //
+
+}
