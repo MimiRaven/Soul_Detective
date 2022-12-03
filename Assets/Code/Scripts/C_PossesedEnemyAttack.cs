6914952ff5cd13ead6d8bf71a1750d64985a1c19
CasasJc <jesuscasas@knights.ucf.edu>
2022-12-03 00:35:24 -0500

    JC Code
    Possessable enemy's animations completed

---
+++ Assets/Code/Scripts/C_PossesedEnemyAttack.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class C_PossesedEnemyAttack : MonoBehaviour
+{
+    public Animator animator;
+
+    [SerializeField]
+    public PlayerInput playerInput;
+
+    private InputAction AttackAction;
+
+    public bool isAttacking;
+
+    AudioSource audioSource;
+    public AudioClip[] audioClipArray;
+
+    [SerializeField]
+    private float animationFinishedTime = 0.9f;
+
+    public bool IsAbleToAttack;
+
+    public void Awake()
+    {
+        AttackAction = playerInput.actions["Attack"];
+    }
+
+    public void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        IsAbleToAttack = true;
+    }
+
+    public void Update()
+    {
+        if (isAttacking && animator.GetCurrentAnimatorStateInfo(1).normalizedTime >= animationFinishedTime)
+        {
+            isAttacking = false;
+        }
+    }
+
+    private void OnEnable()
+    {
+
+        AttackAction.performed += _ => AttackStart();
+        AttackAction.canceled += _ => AttackStop();
+
+    }
+
+    private void OnDisable()
+    {
+
+        AttackAction.performed -= _ => AttackStart();
+        AttackAction.canceled -= _ => AttackStop();
+
+    }
+
+    public void AttackStart()
+    {
+        if (IsAbleToAttack == true)
+        {
+
+            Attacking();
+        }
+
+
+    }
+
+    public void AttackStop()
+    {
+
+    }
+
+
+    public void Attacking()
+    {
+        if (!isAttacking)
+        {
+            animator.SetTrigger("IsAttacking");
+            StartCoroutine(InitialiseAttack());
+
+        }
+    }
+
+    IEnumerator InitialiseAttack()
+    {
+        yield return new WaitForSeconds(0.1f);
+        isAttacking = true;
+    }
+}
