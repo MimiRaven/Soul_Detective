f8a6e50278645f337d3f00a38497094fec8fa13b
CasasJc <jesuscasas@knights.ucf.edu>
2023-02-23 17:39:23 -0500

    JC Code Level 3 puzzles update

--- Assets/C_EnemyLeverPuzzle.cs
+++ Assets/C_EnemyLeverPuzzle.cs
@@ -9,13 +9,15 @@ public class C_EnemyLeverPuzzle : MonoBehaviour
 
     public GameObject Hazard;
 
+    public GameObject PuzzleEnemy;
+
     public C_EnemyPossesed C_EnemyPossesed;
-    public Bot Bot;
+   // public Bot Bot;
     public C_PlayerController C_PlayerController;
     public C_PossesedEnemyAttack C_PossesedEnemyAttack;
 
-    public Behaviour c_EnemyPossesed;
-    public Behaviour c_EnemyAttack;
+  //  public Behaviour c_EnemyPossesed;
+   // public Behaviour c_EnemyAttack;
 
     // Start is called before the first frame update
     void Start()
@@ -35,14 +37,18 @@ public class C_EnemyLeverPuzzle : MonoBehaviour
 
         if (col.collider.tag == "weapon")
         {
+          
            // C_PossesedEnemyAttack.isAttacking = false;
             C_PlayerController.Possesed = true;
             C_EnemyPossesed.Possesed = false;
+           // PuzzleEnemy.SetActive(false);
             Hazard.SetActive(false);
             LeverUp.SetActive(false);
+            PuzzleEnemy.SetActive(false);
             LeverDown.SetActive(true);
-            c_EnemyPossesed.enabled= false;
-            c_EnemyAttack.enabled= false;
+            
+            //c_EnemyPossesed.enabled= false;
+            //c_EnemyAttack.enabled= false;
             //Bot.enabled= false;
         }
     }
