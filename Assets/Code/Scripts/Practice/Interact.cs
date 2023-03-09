c971bc33b48d6b7b9f629e6f154728d5b13b1689
angelapage <angelapage1617@gmail.com>
2023-03-08 21:11:14 -0500

    Code AP - door and typewriter interactions
    Added interactions to the doors so that they will take you to their designated levels. I also added the typewriter interaction which is currently functioning as the desk itself since there's no typewriter model yet.

---
+++ Assets/Code/Scripts/Practice/Interact.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class Interact : MonoBehaviour
+{
+    public bool Level1Range;
+    public bool Level2Range;
+    public bool Level3Range;
+    public bool TypeWriterRange;
+
+    [SerializeField]
+    private PlayerInput playerInput;
+
+    private InputAction Interaction;
+
+    public GameObject SkillTreeCanvas;
+
+    private void Awake()
+    {
+        Interaction = playerInput.actions["Diolouge"];
+    }
+
+    void OnEnable()
+    {
+        Interaction.performed += _ => InteractStart();
+        Interaction.canceled += _ => InteractStop();
+    }
+
+    void OnDisable()
+    {
+        Interaction.performed -= _ => InteractStart();
+        Interaction.canceled -= _ => InteractStop();
+    }
+    void Start()
+    {
+        SkillTreeCanvas.SetActive(false);
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void InteractStart()
+    {
+        if(Level1Range == true)
+        {
+            SceneManager.LoadScene("Level 1");
+        }
+
+        if(Level2Range == true)
+        {
+            SceneManager.LoadScene("Level 2");
+        }
+
+        if(Level3Range == true)
+        {
+            SceneManager.LoadScene("Level 3");
+        }
+
+        if(TypeWriterRange == true)
+        {
+            SkillTreeCanvas.SetActive(true);
+        }
+    }
+
+    public void InteractStop()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Level 1")
+        {
+            Level1Range = true;
+        }
+
+        if (other.tag == "Level 2")
+        {
+            Level2Range = true;
+        }
+
+        if (other.tag == "Level 3")
+        {
+            Level3Range = true;
+        }
+
+        if(other.tag == "Typewriter")
+        {
+            TypeWriterRange = true;
+        }
+    } 
+
+    void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Typewriter")
+        {
+            TypeWriterRange = false;
+        }
+    }
+}
