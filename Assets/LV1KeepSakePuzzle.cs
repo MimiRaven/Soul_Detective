6326d0acdc268ba2b8d524658513e7b37d166d2b
CasasJc <jesuscasas@knights.ucf.edu>
2023-04-23 03:41:18 -0400

    JC code
    lv 1 side quest prog

---
+++ Assets/LV1KeepSakePuzzle.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LV1KeepSakePuzzle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+
+        if (other.CompareTag("LV2End"))
+        {
+           
+
+
+        }
+    }
+}
